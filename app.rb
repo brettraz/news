8e83885f81e434e97b5fee0e619a86e034c54dbc
brettraz <brett.rasmussen@kellogg.northwestern.edu>
2020-02-19 10:31:01 +0000

    app erb

--- app.rb
+++ app.rb
@@ -11,6 +11,28 @@ ForecastIO.api_key = "03d21ffdf385dfb8376360cc6d085591"
 
 get "/" do
   # show a view that asks for the location
+
+   
+    <title>Geocoding</title>
+  </head>
+  <body>
+
+    <div class="container mt-5">
+      <h1>Geocoding</h1>
+      <form action="/map">
+        <input type="text" name="q">
+        <button type="submit">Go there</submit>
+        </form>
+    </div>
+
+    
+  view "app"
+end
+
+get "/map" do
+    results = Geocoder.search(params["q"])
+    lat_long = results.first.coordinates # => [lat, long]
+    "#{lat_long[0]} #{lat_long[1]}"
 end
 
 get "/news" do
