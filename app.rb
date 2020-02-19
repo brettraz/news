2c7712b27692a3aaafc6f944c4f1cd799553e89a
brettraz <brett.rasmussen@kellogg.northwestern.edu>
2020-02-19 13:15:28 +0000

    stuck

--- app.rb
+++ app.rb
@@ -22,9 +22,22 @@ end
 get "/map" do
     results = Geocoder.search(params["q"])
     lat_long = results.first.coordinates # => [lat, long]
-    "#{lat_long[0]} #{lat_long[1]}"
+    "#{lat_long[0]}, #{lat_long[1]}"
+    #coordinates= "#{lat_long[0]}, #{lat_long[1]}"
 end
+
+#  get "/weather" do
+
+# results = Geocoder.search(params["q"])
+# lat_long = results.first.coordinates # => [lat, long]
+#  forecast = ForecastIO.forecast(lat_long[0], lat_long[1]).to_hash
+#  pp forecast
+#  end
 
 get "/news" do
   # do everything else
+url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=cb52b1ae35d447ea9481b307ce9e4799"
+news = HTTParty.get(url).parsed_response.to_hash
+#news = HTTParty.get(url).params["q"].to_hash
+# news is now a Hash you can pretty print (pp) and parse for your output
 end
